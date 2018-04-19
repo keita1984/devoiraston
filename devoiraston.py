9804a39b96e17d616aa24d76299a2cce75560436
keita <keita@yahoo.fr>
2018-04-19 15:15:09 +0200

    ft

---
+++ devoiraston.py
@@ -0,0 +1,119 @@
+'''
+def ex01():
+    text = "je dois faire des sauvegardes régulières de mes dossiers"
+    e = 0
+    while e < 500:
+        print(text)
+        e = e + 1
+ex01()
+'''
+
+'''
+ def ex02():
+        r = ""
+        for i in range(n1, n2 + 1000):
+            if i % 2:
+                r + = str(i) + " "
+        return r
+ex02 ()
+'''
+
+'''
+def ex05():
+    a = int(input( " entrez un nombre : "))
+    if (a % 2 == 0):
+    print("pair")
+    else:
+    print("impair")
+ex05()
+'''
+
+'''
+def ex03():
+    compteur = 1
+    while compteur <=10:
+        print(compteur, "*13 =", compteur *13)
+        compteur +=1
+ex03
+'''
+'''
+def ex04
+    print = ("donne moi un mot")
+    print = (len(valeur))
+ex04
+    
+'''
+'''
+
+def ex10():
+    nbr = int(input("donne l'âge?:"))
+    if (nbr == 6 or nbr == 7)
+          print ("poussin")
+   elif(nbr == 8 or nbr == 9)
+          print ("pupille")
+   elif(nbr == 10 or nbr == 11)
+          print ("minime")
+   elif(nbr > 12)
+          print ("cadet")
+   else
+          print ("too small")
+ex10
+'''
+
+'''
+def ex06():
+     nbr = int(input("ecrire un nombre entier entre 10 et 20 :"))
+    if (nbr > 20):
+      print text ("inferieur")
+    elif(nbr <10):
+      print text ("superieur")
+ex06()
+'''
+'''
+def ex7():
+    nbr = int(input("ecrire un nombre :"))
+    print(nbr + 1)
+    print(nbr + 2)
+    print(nbr + 3)
+    print(nbr + 4)
+    print(nbr + 5)
+    print(nbr + 6)
+    print(nbr + 7)
+    print(nbr + 8)
+    print(nbr + 9)
+    print(nbr + 10)
+ex07()
+'''
+'''
+def ex08():
+    nbr = int(input("ecrire un chiffre:"))
+    print(nbr * 1)
+    print(nbr * 2)
+    print(nbr * 3)
+    print(nbr * 4)
+    print(nbr * 5)
+    print(nbr * 6)
+    print(nbr * 7)
+    print(nbr * 8)
+    print(nbr * 9)
+    print(nbr * 10)
+e08()
+'''
+'''
+ def ex09():
+     age = int(input("quel est l'age de votre bebe?"))
+ex09()
+'''
+'''
+def ex11():
+    nbr_article = int(input("nbre d'article pose?:"))
+    prix _article = float(input("quel est le prix HT unitaire de l'article?:"))
+    prixht = prix article * nbr_article
+    print ("nombres d'articles : ",nbr_article)
+    print ("prix HT : ",printht)
+    tva = (prixht * 1.2)
+    print ("prix TTC:", tva)
+ex11
+'''
+
+
